ce8d7c97067f7e98897b798bd5fd49e2817bcc69
Enakarhire Ovie Louis <enakarhireovie7@gmail.com>
2023-05-22 07:39:57 -0700

    UNIX is basically a simple operating system

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - prints a line to the standard error
+ *main - Entry point
  *Description: 'simple operating system'
  *Return: 1 (Success)
  */
