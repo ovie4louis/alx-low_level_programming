df663b8da3780a69a4099613efec86d8291db2c3
Enakarhire Ovie Louis <enakarhireovie7@gmail.com>
2023-05-29 12:21:51 -0700

    7. Winning is only half of it

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -17,16 +17,16 @@ void puts_half(char *str)
 	}
 	if (j % 2 == 1)
 	{
-		k = (j -1) / 2;
-		k += 1;
+	k = (j -1) / 2;
+	k += 1;
 	}
 	else
 	{
-		k = j / 2;
+	k = j / 2;
 	}
 	for (; k < j; k++)
 	{
-		_putchar(str[k]);
+	_putchar(str[k]);
 	}
 	_putchar('\n');
 }
