d1c7f8c1781a5d6442a415e8a119cf043a924e3d
Enakarhire Ovie Louis <enakarhireovie7@gmail.com>
2023-06-08 15:37:28 -0700

    0x08. C - Recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -14,7 +14,7 @@ int _sqrt_recursion(int n)
 	return (actual_sqrt_recursion(n, 0));
 }
 /**
- *actual_sqrt_recursionn- recurses to find the natural
+ *actual_sqrt_recursion - recurses to find the natural
  *square root of a number
  *@n: number to calculate the square root of
  *@i:iterator
