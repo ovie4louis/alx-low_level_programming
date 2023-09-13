7ad562cbae18d5ff40dbad526238da30c6891052
Enakarhire Ovie Louis <enakarhireovie7@gmail.com>
2023-09-13 01:41:36 -0700

    dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,9 +9,9 @@
 */
 struct dog
 {
-	char name;
+	char *name;
 	float age;
-	char owner;
+	char *owner;
 };
 
 /**
@@ -20,7 +20,7 @@ struct dog
 
 typedef struct dog dog_t;
 
-void init_dog(struct dog *d, char *name, float age, char *owner);
+void struct init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
