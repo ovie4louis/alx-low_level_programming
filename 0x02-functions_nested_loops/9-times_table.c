01d2ad6518e8fe6667955d3f5d1559126637d51b
Enakarhire Ovie Louis <enakarhireovie7@gmail.com>
2023-10-01 12:34:32 -0700

    task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,16 +7,19 @@
 
 void times_table(void)
 {
-	int i = 9;
-	int j = 0;
-	int multi;
-	int k;
+	int a = 0;
+	int b = 0;
+	int d;
 
-	for (k = 0; k <= 12; k++)
+	for (a = 0; a <= 9; a++)
 	{
-	multi = i * j;
-	printf("%d", multi);
+	for (b = 0; b <= 9; b++)
+	{
+	d = a * b;
+	printf("%d", d);
+	printf(",");
+	printf(" ");
+	}
 	printf("\n");
-	++j;
 	}
 }
