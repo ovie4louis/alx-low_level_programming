678fadf2c46a67ce6a52362237e004c0bbb1d059
Enakarhire Ovie Louis <enakarhireovie7@gmail.com>
2023-10-02 09:37:57 -0700

    task 9

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -8,8 +8,8 @@
 
 int add(int a, int b)
 {
-	int a = 1;
-	int b = 1;
+	a;
+	b;
 	int result;
 
 	result = add(a, b);
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,19 +7,34 @@
 
 void times_table(void)
 {
-	int a = 0;
-	int b = 0;
-	int d;
+	int x, y, z, d;
 
-	for (a = 0; a <= 9; a++)
+	for (x = 0; x <= 9; x++)
 	{
-	for (b = 0; b <= 9; b++)
+	for (y = 0; y <= 9; y++)
 	{
-	d = a * b;
-	printf("%d", d);
-	printf(",");
-	printf("   ");
+	z = x * y;
+	if (z > 9)
+	{
+	u = z % 10;
+	d = (z - u) / 10;
+	_putchar(44);
+	_putchar(32);
+	_putchar(d + '0');
+	_putchar(u + '0');
 	}
-	printf("\n");
+	else
+	{
+	if (y != 0)
+	{
+	_putchar(44);
+	_putchar(32);
+	_putchar(32);
 	}
+	_putchar(z + '0');
+	}
+	}
+	_putchar('\n');
+	}
+}
 }
