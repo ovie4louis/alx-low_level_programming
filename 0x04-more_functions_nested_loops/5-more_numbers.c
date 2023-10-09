6c4ad4e7be9e9f0f1f2f11ee7f6ff67f8b8b7033
Enakarhire Ovie Louis <enakarhireovie7@gmail.com>
2023-10-09 10:49:44 -0700

    task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,15 +6,20 @@
 
 void more_numbers(void)
 {
-	int i, j;
-	for (i = 1; i <= 10; i++)
-	{ 
-		for (j = 0; j <=14; j++)
-		{
-			if (j >= 10)
-				_putchar('1');
-			_putchar (j % 10 + '0');
-		}
-		_putchar('\n');
+	int num, row, count;
+
+	for (row = 1; row <= 10; row++)
+	{
+	for (count = 0; count <= 14; count++)
+	{
+	num = count;
+	if (count > 9)
+	{
+	_putchar(1 + 48);
+	num = count % 10;
+	}
+	_putchar(num + 48);
+	}
+	_putchar('\n');
 	}
 }
