a764419f04dcadb9742707fc721e7e9c01ac7bc3
Enakarhire Ovie Louis <enakarhireovie7@example.com>
2023-11-03 08:54:26 -0700

    task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -21,9 +21,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	s2 = "";
 	for (s1_length = 0; s1[s1_length] != '\0'; s1_length++)
 	;
-	for (s2_length == 0; s2[s2_length] != '\0'; s2_lenth++)
+	for (s2_length = 0; s2[s2_length] != '\0'; s2_length++)
 	;
-
 	str = malloc(s1_length + n + 1);
 	if (str == NULL)
 {
