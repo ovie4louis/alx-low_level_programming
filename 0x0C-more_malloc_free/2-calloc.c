4ef5f9d69f0429ac716562acd8e8ecbc0d2d1652
Enakarhire Ovie Louis <enakarhireovie7@example.com>
2023-11-03 08:57:04 -0700

    task 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -31,7 +31,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (size == 0 || nmemb == 0)
 	return (NULL);
-	m malloc(sizeof(int) * nmemb);
+	m =  malloc(sizeof(int) * nmemb);
 
 	if (m == 0)
 	return (NULL);
